6ed5e7f8b99b852af834cb565ef6986b90c0b405
Dioni Alves <dionialves@gmail.com>
2025-03-03 21:30:14 -0300

    Added sql file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_category (name) VALUES ('Livros');
+INSERT INTO tb_category (name) VALUES ('Eletronicos');
+
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('Senhor dos Aneis', 'Baita livro', 300.00, 'https://livros.com');
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('Biblia Sagrada', 'O melhor de todos', 250.00, 'https://biblia.com');
+
